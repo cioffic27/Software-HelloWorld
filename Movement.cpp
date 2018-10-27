8272af7ec5df0bad2540964bbd2700ce31aa46da
JKTison <31457389+JKTison@users.noreply.github.com>
2018-10-27 01:21:06 -0400

    Add files via upload
    At least with my Visual Studio, I need to have pch.h in order to compile and run SoftDesGame 1.5.cpp
    
    Running into an issue with the Movement functions always returning to North before you can exit the game, but the Movement constructor is done and tests have it working right.

---
+++ Movement.cpp
@@ -0,0 +1,260 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Movement {
+public:
+	string north_des= "";
+	string south_des = "";
+	string east_des = "";
+	string west_des= "";
+	string center_des= "";
+	string input = "";
+
+	Movement(string n, string s, string e, string w, string c)
+	{
+		north_des = n;
+		south_des = s;
+		east_des = e;
+		west_des = w;
+		center_des = c;
+	}
+
+	void North( Movement test) 
+	{
+		cout << "You are in the north end of the room." << endl;
+		cout << north_des << endl;
+
+		while (input != "Exit") 
+		{
+			cin >> input;
+
+			if (input == "North") 
+			{
+				cout << "You are already north." << endl;
+			}
+			
+			if (input == "South") 
+			{
+				cout << "You go South" << endl;
+				test.South(test);
+			}
+
+			if (input == "Center")
+			{
+				cout << "You go to the Center" << endl;
+				test.Center(test);
+			}
+
+			if (input == "East") 
+			{
+				cout << "You go East" << endl;
+				test.East(test);
+			}
+
+			if (input == "West") 
+			{
+				cout << "You go West" << endl;
+				test.West(test);
+			}
+
+			if (input == "Search") {
+				cout << north_des << endl;
+			}
+		}
+
+	}
+
+	void South(Movement test) 
+	{
+		cout << "You are in the south end of the room." << endl;
+		cout << south_des << endl;
+
+		while (input != "Exit") 
+		{
+			cin >> input;
+
+			if (input == "North") 
+			{
+				cout << "You go North" << endl;
+				test.North(test);
+			}
+
+			if (input == "South") 
+			{
+				cout << "You are already South" << endl;
+			}
+
+			if (input == "Center")
+			{
+				cout << "You go to the Center" << endl;
+				test.Center(test);
+			}
+
+			if (input == "East") 
+			{
+				cout << "You go East" << endl;
+				test.East(test);
+			}
+
+			if (input == "West") 
+			{
+				cout << "You go West" << endl;
+				test.West(test);
+			}
+
+			if (input == "Search") 
+			{
+				cout << south_des << endl;
+			}
+		}
+	}
+
+	void East(Movement test) 
+	{
+		cout << "You are in the east end of the room." << endl;
+		cout << east_des << endl;
+
+		while (input != "Exit") 
+		{
+			cin >> input;
+			if (input == "North") 
+			{
+				cout << "You go North" << endl;
+				test.North(test);
+			}
+
+			if (input == "South") 
+			{
+				cout << "You go South" << endl;
+				test.South(test);
+			}
+			
+			if (input == "Center")
+			{
+				cout << "You go to the Center" << endl;
+				test.Center(test);
+			}
+
+			if (input == "East") 
+			{
+				cout << "You are already east" << endl;				
+			}
+
+			if (input == "West") 
+			{
+				cout << "You go West" << endl;
+				test.West(test);
+			}
+
+			if (input == "Search") 
+			{
+				cout << east_des << endl;
+			}
+		}
+	}
+
+	void West(Movement test) 
+	{
+		cout << "You are in the west end of the room." << endl;
+		cout << west_des << endl;
+
+		while (input != "Exit") 
+		{
+			cin >> input;
+
+			if (input == "North") 
+			{
+				cout << "You go North" << endl;
+				test.North(test);
+			}
+
+			if (input == "South") 
+			{
+				cout << "You go South" << endl;
+				test.South(test);
+			}
+
+			if (input == "Center")
+			{
+				cout << "You go to the Center" << endl;
+				test.Center(test);
+			}
+
+			if (input == "East") 
+			{
+				cout << "You go East" << endl;
+				test.East(test);
+			}
+
+			if (input == "West") 
+			{
+				cout << "You are already West" << endl;
+			}
+
+			if (input == "Search") 
+			{
+				cout << west_des << endl;
+			}
+		}
+	}
+
+	void Center(Movement test) 
+	{
+		cout << "You are in the center of the room." << endl;
+		cout << center_des << endl;
+
+		while (input != "Exit") 
+		{
+			cin >> input;
+
+			if (input == "North") 
+			{
+				cout << "You go North" << endl;
+				test.North(test);
+			}
+
+			if (input == "South") 
+			{
+				cout << "You go South" << endl;
+				test.South(test);
+			}
+
+			if (input == "Center")
+			{
+				cout << "You are already in the Center" << endl;
+			}
+
+			if (input == "East") 
+			{
+				cout << "You go East" << endl;
+				test.East(test);
+			}
+
+			if (input == "West") 
+			{
+				cout << "You go West" << endl;
+				test.West(test);
+			}
+
+			if (input == "Search") 
+			{
+				cout << center_des << endl;
+			}
+		}
+	}
+};
+
+
+int main() {
+	Movement room2
+		("This is North\n\n", 
+		"This is South\n\n", 
+		"This is East\n\n",
+		"This is West\n\n",
+		"This is Center\n\n");
+		
+	room2.North(room2);
+}
+
